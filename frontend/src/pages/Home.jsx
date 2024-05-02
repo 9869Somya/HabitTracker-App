3230206326733b91f798290269b51cb85e797461
Somya Parida <paridasomya2468@gmail.com>
2024-05-03 00:41:31 +0530

    Modified some css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='homePage'>
-      <div className='background pt-3'>
-        <h1 className='text-center text-light mt-4'>
+    <div className="homepage">
+      <div className="background pt-3">
+        <h1 className="text-center text-light mt-4">
           Build Better Habits, Build a Better Life
         </h1>
-        <p className='text-center text-light'>
+        <p className="text-center text-light">
           Harness the power of our personalized habit tracker app to streamline
           your everyday routines and achieve your goals.
         </p>
         <br />
         <div className="d-flex justify-content-center">
-          <button type="button" className='btn btn-success'>
+          <button type="button" className="btn btn-success">
             Add Habit
           </button>
         </div>
@@ -22,9 +22,9 @@ const Home = () => {
           Organize your chaos, execute your plan, and triumph over adversity.
         </h1>
         <p className="text-center text-light pb-4">
-          Start your day right with a clear, organized schedule that keeps you on
-          track for success. Here’s an example of how your day with our website
-          could look:
+          Start your day right with a clear, organized schedule that keeps you
+          on track for success. Here’s an example of how your day with our
+          website could look:
         </p>
         <div className="d-flex justify-content-center py-3">
           <img
@@ -40,47 +40,62 @@ const Home = () => {
             style={{ width: "400px", opacity: 0.8 }}
           />
         </div>
-      
-      <h1 className='text-center text-light my-4'>
-        Stay Empowered by Your Progress
-      </h1>
-      <div className='d-flex justify-content-center'>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
+
+        <h1 className="text-center text-light my-4">
+          Stay Empowered by Your Progress
+        </h1>
+        <div className="d-flex justify-content-center">
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
         </div>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
-        </div>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
-        </div>
-      </div>
-      <div className='d-flex justify-content-center'>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
-        </div>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
-        </div>
-        <div className="card mx-3 my-4" style={{ width: "18rem" }}>
-          <img src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
-            className="card-img-top"
-            alt="Exercise" />
-        </div>
+        <div className="d-flex justify-content-center">
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
+          <div className="card mx-3 my-4" style={{ width: "18rem" }}>
+            <img
+              src="https://www.pngmart.com/files/21/Exercise-PNG-Isolated-Free-Download.png"
+              className="card-img-top"
+              alt="Exercise"
+            />
+          </div>
         </div>
       </div>
       <div>
-        <footer className='text-center text-light' style={{ backgroundColor: '#153448', padding: '40px' }}>
+        <footer
+          className="text-center text-light"
+          style={{ backgroundColor: "#153448", padding: "40px" }}
+        >
           <h3>Contact Us</h3>
           <p>Email: contact@example.com</p>
           <p>Phone: +1234567890</p>
@@ -89,6 +104,6 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Home;
--- frontend/src/pages/StreakDetails.jsx
+++ frontend/src/pages/StreakDetails.jsx
@@ -6,7 +6,7 @@ import StreakCard from "../components/StreakCard";
 const StreakDetails = () => {
   const { id } = useParams();
   const [streaks, setStreaks] = useState([]);
-  const [showMessage, setShowMessage] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
 
   async function getStreakData(id) {
     let res = await habitApiService.getStreakLogsById(id);
@@ -22,42 +22,43 @@ const StreakDetails = () => {
   if (!streaks) return null;
 
   return (
-    <div className="container">
+    <div style={{ background: "rgba(0, 0, 0, 0.5)" }}>
       <div className="mb-4">
         <button
           className="btn btn-sm btn-light"
           onClick={() => setShowMessage(!showMessage)}
+          style={{
+            width: "15%",
+            backgroundColor: "#088F8F",
+            color: "white",
+            padding: "10px",
+          }}
         >
-          {showMessage ? "Hide" : "Reminder"}
+          {showMessage ? "Hide up" : "Show tip"}
         </button>
       </div>
-      {showMessage && (
-        <div className="alert alert-info" role="alert">
-          <p>
-            <strong>Building a habit takes consistency.</strong> To truly solidify your habit,
-            aim to maintain a streak of continuous completion that matches its
-            frequency. Until you achieve this streak, consider your habit a
-            work in progress. Stay committed, and you'll soon see the results
-            you desire!
-          </p>
-        </div>
-      )}
-      <h1
-        className="text-center mt-5 mb-4"
-        style={{
-          color: "#153448",
-          fontFamily: "initial",
-          fontSize: "60px",
-        }}
-      >
-        Make today count with Daily Habits !!
-      </h1>
-      <div className="row">
-        {streaks.map((streak) => (
-          <div className="col-md-3 mb-4" key={streak._id}>
-            <StreakCard streak={streak} habitId={id} />
+      <div className="container" id="streakDetail">
+        {showMessage && (
+          <div className="alert alert-info" role="alert">
+            <p>
+              <strong>Building a habit takes consistency.</strong> To truly
+              solidify your habit, aim to maintain a streak of continuous
+              completion that matches its frequency. Until you achieve this
+              streak, consider your habit a work in progress. Stay committed,
+              and you'll soon see the results you desire!
+            </p>
           </div>
-        ))}
+        )}
+        <h1 className="text-center mt-5 mb-4" id="streak_header">
+          Daily Habit Streaks !!
+        </h1>
+        <div className="row">
+          {streaks.map((streak) => (
+            <div className="col-md-3 mb-4" key={streak._id}>
+              <StreakCard streak={streak} habitId={id} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
